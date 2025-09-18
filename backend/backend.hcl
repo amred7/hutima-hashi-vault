157636a6ea7b0f94dfe3dd49385c7ea8f37fc223
reda akachat <reda@akachat.net>
2025-09-18 14:17:44 -0400

    added backend v0.4

---
+++ backend/backend.hcl
@@ -0,0 +1,5 @@
+
+storage_account_name = "tfmpnprodcacnsa"
+container_name       = "tfstate_vault"
+key                  = "terraform_vault.tfstate"
+
--- backend/backend.tf
+++
@@ -1,7 +0,0 @@
-terraform {
-  backend "azurerm" {
-    storage_account_name = "tfmpnprodcacnsa"
-    container_name       = "tfstate_vault"
-    key                  = "terraform_vault.tfstate"
-  }
-}
