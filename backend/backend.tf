2074fb2946bc7a7b621b1ad1eea2ba597d2c41a8
reda akachat <reda@akachat.net>
2025-09-18 14:14:46 -0400

    added backend v0.3

--- backend/backend.tf
+++ backend/backend.tf
@@ -1,9 +1,7 @@
 terraform {
   backend "azurerm" {
-    access_key           = var.sa_acces_key
     storage_account_name = "tfmpnprodcacnsa"
     container_name       = "tfstate_vault"
     key                  = "terraform_vault.tfstate"
-
   }
 }
